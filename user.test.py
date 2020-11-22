6dc6339ce691b595d7240240654c06e556f4ac7c
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-22 21:16:33 +0300

    create find credentials

--- user.test.py
+++ user.test.py
@@ -26,10 +26,10 @@ class TestCredentials(unittest.TestCase):
     def tearDown(self):
         Credentials.user_credentials =[]
 
-    # def test_details(self):
-    #     self.assertEqual(self.new_credentials.acc_name,"facebook")
-    #     self.assertEqual(self.new_credentials.user_name,"fridah")
-    #     self.assertEqual(self.new_credentials.password,"1234567")
+    def test_details(self):
+        self.assertEqual(self.new_credentials.acc_name,"facebook")
+        self.assertEqual(self.new_credentials.user_name,"fridah")
+        self.assertEqual(self.new_credentials.password,"1234567")
 
 
     def test_save_credentials(self):
@@ -38,13 +38,13 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(len(Credentials.user_credentials),1)
 
 
-    def find_credentials(self):
-        self.new_credentials.save_credentials()
-        test_credential = Credentials("acc_name","user_name","password")
-        test_credential.save_credentials() 
+    # def find_credentials(self):
+    #     self.new_credentials.save_credentials()
+    #     test_credential = Credentials("acc_name","user_name","password")
+    #     test_credential.save_credentials() 
 
-        the_credential =Credentials.find_acc_name("acc_name")
-        self.assertEqual(the_credential.acc_name,test_credential.acc_name)
+    #     the_credential =Credentials.find_acc_name("acc_name")
+    #     self.assertEqual(the_credential.acc_name,test_credential.acc_name)
 
 
     def test_exist(self):
